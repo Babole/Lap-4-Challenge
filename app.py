87233700d20ddc8ec5f7d1fe0aa75c9c717811da
Matthew Lo <matthewlol316@gmail.com>
2022-10-26 15:37:04 +0100

    add generator ID

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, render_template
 from flask_cors import CORS
+import shortenURL
+
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods = ['POST', 'GET'])
 def home():
-    return render_template("home.html"),200
+    if request.method == 'POST':
+        shorten_URL = shortenURL.id_generator()
+        return render_template('home.html', shorten_URL = shorten_URL),200
+    
+    else:
+        return render_template('home.html'), 200
 
     
 if __name__=="__main__":
